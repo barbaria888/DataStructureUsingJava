e3ba6d835915e76ffc53cb66120304f4096a2781
HARDIK ARORA <122469904+barbaria888@users.noreply.github.com>
2024-05-01 19:14:07 +0530

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
      public static void main(String[] args) {
          Scanner sc= new Scanner(System.in);
          int n=sc.nextInt();
-         char x='A';
          int arr[][]=new int[n][n];
          for(int i=0;i<n;i++)
          {
